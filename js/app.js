dee78c4d3cd92dd631c418f2e6ad0bf6e23e51cd
shaynaCummings <shayna.cummings@gmail.com>
2014-12-14 16:59:00 -0500

    added boilerplate to make sure things link up

---
+++ js/app.js
@@ -0,0 +1,23 @@
+angular.module('PetAppUI', [
+    'ngRoute'
+]);
+
+angular.module('PetAppUI').config(function($routeProvider) {
+    'use strict';
+
+    $routeProvider
+        .when('/', {
+            templateUrl: 'templates/home.html'
+        })
+        .when('/dashboard', {
+            templateUrl: 'templates/dashboard.html',
+            controller: 'DashboardCtrl'
+        })
+        .when('/record', {
+            templateUrl: 'templates/record.html',
+            controller: 'RecordCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
