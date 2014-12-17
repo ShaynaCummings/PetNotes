2cacee94987718ecb967b582c6396e4470b7166b
shaynaCummings <shayna.cummings@gmail.com>
2014-12-17 13:31:32 -0500

    deal with user controller later

--- js/controllers/user.controller.js
+++ js/controllers/user.controller.js
@@ -1,19 +1,19 @@
-//testing
+//finish later
 
-angular.module('PetAppUI').controller('UserCtrl', function ($scope, $http, $window) {
-  $scope.user = {username: 'john.doe', password: 'foobar'};
-  $scope.message = '';
-  $scope.submit = function () {
-    $http
-      .post('/authenticate', $scope.user)
-      .success(function (data, status, headers, config) {
-        $window.sessionStorage.token = data.token;
-        $scope.message = 'Welcome';
-      })
-      .error(function (data, status, headers, config) {
-        delete $window.sessionStorage.token;
+// angular.module('PetAppUI').controller('UserCtrl', function ($scope, $http, $window) {
+//   $scope.user = {username: 'john.doe', password: 'foobar'};
+//   $scope.message = '';
+//   $scope.submit = function () {
+//     $http
+//       .post('/authenticate', $scope.user)
+//       .success(function (data, status, headers, config) {
+//         $window.sessionStorage.token = data.token;
+//         $scope.message = 'Welcome';
+//       })
+//       .error(function (data, status, headers, config) {
+//         delete $window.sessionStorage.token;
 
-        $scope.message = 'Error: Invalid user or password';
-      });
-  };
-});
+//         $scope.message = 'Error: Invalid user or password';
+//       });
+//   };
+// });
