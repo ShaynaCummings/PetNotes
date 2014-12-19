c91bfd44bbc6fe695b6fe50207c4d231f9ff610a
shaynaCummings <shayna.cummings@gmail.com>
2014-12-19 02:05:34 -0500

    added buttons for new weight and height

--- js/controllers/lengths.controller.js
+++ js/controllers/lengths.controller.js
@@ -1,16 +1,16 @@
 angular.module('PetAppUI').controller('LengthCtrl', function($scope, $http, $routeParams, ServerUrl) {
     'use strict';
 
-    // $scope.petId = $routeParams.petId;
+    $scope.petId = $routeParams.petId;
     $scope.lengthId = $routeParams.lengthId
 
       $scope.createLength = function(length) {
       var params = {
-          // pet: pet
+          pet: pet
           length: length
       };
 
-      $http.post(ServerUrl + '/pets', params)
+      $http.post(ServerUrl + '/pets' + pet.id + '/lengths', params)
         .success(function(response) {
             $scope.lengths.push(response);
       });
